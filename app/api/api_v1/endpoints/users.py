20da20d36dd30ac4d6105948542c393ab2b09226
Vyacheslav Yermakov <vyacheslav.yermakov@exactprosystems.com>
2022-05-18 01:24:58 +0300

    Add users and wallets endpoints

--- app/api/api_v1/endpoints/users.py
+++ app/api/api_v1/endpoints/users.py
@@ -1,23 +1,38 @@
+from hashlib import md5
+
 from fastapi import APIRouter
 from pydantic import BaseModel, EmailStr
+
+from app.schemas.user import User, UserCreate, UserInDB
 
 router = APIRouter()
 
 users = []
 
+last_id = 0
 
-class User(BaseModel):
-    name: str
-    password: str
-    email: EmailStr
+
+def gen_id() -> int:
+    global last_id
+    last_id += 1
+    return last_id
 
 
 @router.post("/users")
-async def create_user(name: str, password: str, email: EmailStr):
-    print(name, password, email)
-    users.append(User(name=name, password=password, email=email))
+async def create_user(user_create: UserCreate):
+    users.append(UserInDB(name=user_create.name,
+                          email=user_create.email,
+                          id=gen_id(),
+                          hashed_password=md5(user_create.password.encode()).hexdigest()
+                          ))
 
 
 @router.get("/users")
 async def get_users():
+    return users
+
+
+@router.get("/users/{user_id}")
+async def get_user(user_id):
+    """Returns the concrete user from DB."""
     return users
